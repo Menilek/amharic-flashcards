e0ef79f2a31c20f2c770303a0de55ff64b9b21e7
Menilek Techane <menilek.techane@ultromics.com>
2021-12-08 22:44:55 +0000

    Create Days & Months components

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import Days from './components/Days.vue'
+import Months from './components/Months.vue'
+</script>
+
+<template>
+  <Days />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+  <Months />
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/utils/util.ts
@@ -0,0 +1,25 @@
+export const amharicDays = {
+    "Monday": "Segno",
+    "Tuesday": "Makisegno",
+    "Wednesday": "Erob",
+    "Thursday": "Hamus",
+    "Friday": "Arb",
+    "Saturday": "Kidame",
+    "Sunday": "Ehud"
+}
+
+export const amharicMonths = {
+    "September": "Meskerem",
+    "October": "Tikimt",
+    "November": "Hidar",
+    "December": "Tahsas",
+    "January": "Tir",
+    "February": "Yekatit",
+    "March": "Megabit",
+    "April": "Miyazya",
+    "May": "Ginbot",
+    "June": "Sene",
+    "July": "Hamle",
+    "August": "Nehase",
+    "The 13th Month": "Pagume"
+}
