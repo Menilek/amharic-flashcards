ec2e8fb1f9a6210d7a740044340f6967d5ab4727
Menilek Techane <menilek.techane@ultromics.com>
2021-12-08 23:32:19 +0000

    Remove duplicated CSS

--- src/components/Days.vue
+++ src/components/Days.vue
@@ -1,14 +1,17 @@
 <script setup lang="ts">
 
-import { Carousel, Slide, Pagination } from 'vue3-carousel';
+import { Carousel, Slide, Pagination, Navigation } from 'vue3-carousel';
+
 import 'vue3-carousel/dist/carousel.css';
-import { amharicDays } from '../utils/util'
+import '../assets/carousel.css';
+import { Days } from '../utils/util'
 
 </script>
 
 <template>
-    <Carousel :itemsToShow="3.95" :wrapAround="true">
-        <Slide v-for="day of amharicDays" :key="day">
+    <h2>Days</h2>
+    <Carousel :itemsToShow="1.5" :wrapAround="true">
+        <Slide v-for="day of Days" :key="day">
             <div class="carousel__item">{{ day }}</div>
         </Slide>
 
@@ -20,22 +23,4 @@ import { amharicDays } from '../utils/util'
 </template>
 
 <style scoped>
-.carousel__slide > .carousel__item {
-  transform: scale(1);
-  opacity: 0.5;
-  transition: 0.5s;
-}
-.carousel__slide--visible > .carousel__item {
-  opacity: 1;
-  transform: rotateY(0);
-}
-.carousel__slide--next > .carousel__item {
-  transform: scale(0.9) translate(-10px);
-}
-.carousel__slide--prev > .carousel__item {
-  transform: scale(0.9) translate(10px);
-}
-.carousel__slide--active > .carousel__item {
-  transform: scale(1.1);
-}
 </style>
--- src/components/Months.vue
+++ src/components/Months.vue
@@ -1,41 +1,25 @@
 <script setup lang="ts">
 
-import { Carousel, Slide, Pagination } from 'vue3-carousel';
+import { Carousel, Slide, Pagination, Navigation } from 'vue3-carousel';
 import 'vue3-carousel/dist/carousel.css';
-import { amharicMonths } from '../utils/util'
+import '../assets/carousel.css';
+import { Months } from '../utils/util'
 
 </script>
 
 <template>
-    <Carousel :itemsToShow="3.95" :wrapAround="true">
-        <Slide v-for="month of amharicMonths" :key="month">
+    <h2>Months</h2>
+    <Carousel :itemsToShow="1.5" :wrapAround="true">
+        <Slide v-for="month of Months" :key="month">
             <div class="carousel__item">{{ month }}</div>
         </Slide>
 
         <template #addons>
-      <Navigation />
-      <Pagination />
-    </template>
+            <Navigation />
+            <Pagination />
+        </template>
     </Carousel>
 </template>
 
 <style scoped>
-.carousel__slide > .carousel__item {
-  transform: scale(1);
-  opacity: 0.5;
-  transition: 0.5s;
-}
-.carousel__slide--visible > .carousel__item {
-  opacity: 1;
-  transform: rotateY(0);
-}
-.carousel__slide--next > .carousel__item {
-  transform: scale(0.9) translate(-10px);
-}
-.carousel__slide--prev > .carousel__item {
-  transform: scale(0.9) translate(10px);
-}
-.carousel__slide--active > .carousel__item {
-  transform: scale(1.1);
-}
 </style>
