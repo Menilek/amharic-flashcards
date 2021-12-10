d9ef3c517b48695a9cc931056e156bbc2665cc8b
Menilek Techane <menilek25@hotmail.co.uk>
2021-12-10 23:25:32 +0000

    Create interfaces for Digits and Numbers

---
+++ src/utils/types.ts
@@ -0,0 +1,28 @@
+export interface DigitsType {
+    Zero: string;
+    One: string;
+    Two: string;
+    Three: string;
+    Four: string;
+    Five: string;
+    Six: string;
+    Seven: string;
+    Eight: string;
+    Nine: string;
+    Ten: string;
+}
+
+export interface NumbersType {
+    Ten: string;
+    Twenty: string;
+    Thirty: string;
+    Forty: string;
+    Fifty: string;
+    Sixty: string;
+    Seventy: string;
+    Eighty: string;
+    Ninety: string;
+    Hundred: string;
+    Thousand: string;
+    Million: string;
+}
