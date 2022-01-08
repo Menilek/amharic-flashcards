a481ed89ba48066127040a31344e44e5dd6fe2c3
Menilek Techane <menilek25@hotmail.co.uk>
2022-01-08 22:47:32 +0000

    Render Carousel view & add styles

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,9 @@
 <script setup lang="ts">
-
-import Days from './components/Days.vue'
-import Months from './components/Months.vue'
-import Seasons from './components/Seasons.vue'
-import Digits from './components/Digits.vue'
-import Numbers from './components/Numbers.vue'
-
-
+  import Carousel from './views/Carousel.vue'
 </script>
 
 <template>
-  <Days />
-  <Months />
-  <Seasons />
-  <Digits />
-  <Numbers />
+  <Carousel />
 </template>
 
 <style>
---
+++ src/views/Carousel.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+
+import Days from '../components/Days.vue'
+import Months from '../components/Months.vue'
+import Seasons from '../components/Seasons.vue'
+import Digits from '../components/Digits.vue'
+import Numbers from '../components/Numbers.vue'
+
+</script>
+
+<template>
+  <Days />
+  <Months />
+  <Seasons />
+  <Digits />
+  <Numbers />
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  /* margin-top: 60px; */
+}
+</style>
